cab66d903ca1a23c411ea99fd574d661fdd713a8
Andrey Arthur <andrey_arthur1@outlook.com>
2024-07-03 11:21:46 -0300

    add eslint and editorconfig rules

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+export default [
+    {
+        rules: {
+            semi: 'error',
+            quotes: ['error', 'single'],
+            indent: ['error', 4],
+            'comma-dangle': ['error', 'always-multiline'],
+        },
+    },
+];
